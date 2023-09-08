a6c2babe9fa1429b3d28d74d4a264604fa330c2c
Colin2222 <crlew@outlook.com>
2023-09-08 14:40:23 -0400

    interweave references between tiles and edges

--- board_setup.py
+++ board_setup.py
@@ -33,9 +33,52 @@ def load_board(board: CatanBoard, setup_file_location, baseX, baseY):
         edge5.adjacent_tiles.append(board.resource_tiles[0][tile_x])
         edge0.adjacent_tiles.append(board.resource_tiles[0][tile_x])
     for tile_y in range(0, height):
-        for tile_x in range(width):
-            if(tile_y % 2 == 0):
+        for tile_x in range(width + 1):
+            # add vertical edges
+            if tile_y % 2 == 0:
                 edge4 = ResourceTileEdge(baseX - 3 + 104 * tile_x, baseY + 48 + 74 * tile_y)
             else:
                 edge4 = ResourceTileEdge(baseX - 3 + 54 + 104 * tile_x, baseY + 48 + 74 * tile_y)
+            if tile_x < width:
+                board.resource_tiles[tile_y][tile_x].edges[4] = edge4
+            if tile_x - 1 > 0:
+                board.resource_tiles[tile_y][tile_x - 1].edges[1] = edge4
 
+        for tile_x in range(width):
+            # add diagonal edges
+            if tile_y % 2 == 0:
+                edge3 = ResourceTileEdge(baseX + 21 + 104 * tile_x, baseY + 85 + 74 * tile_y)
+                edge2 = ResourceTileEdge(baseX + 76 + 104 * tile_x, baseY + 85 + 74 * tile_y)
+
+                # handle edge case of open facing edge on end of array
+                if tile_x == 0 and tile_y != 0:
+                    edge5 = ResourceTileEdge(baseX + 21 + 104 * tile_x, baseY + 11 + 74 * tile_y)
+                    board.resource_tiles[tile_y][tile_x].edges[5] = edge5
+
+                # set tile edges
+                board.resource_tiles[tile_y][tile_x].edges[2] = edge2
+                board.resource_tiles[tile_y][tile_x].edges[3] = edge3
+
+                # set adjacent tile edges
+                if tile_y != height - 1:
+                    board.resource_tiles[tile_y + 1][tile_x].edges[5] = edge2
+                    if tile_x != 0:
+                        board.resource_tiles[tile_y + 1][tile_x - 1].edges[0] = edge3
+            else:
+                edge3 = ResourceTileEdge(baseX + 76 + 104 * tile_x, baseY + 85 + 74 * tile_y)
+                edge2 = ResourceTileEdge(baseX + 131 + 104 * tile_x, baseY + 85 + 74 * tile_y)
+
+                # handle edge case of open facing edge on end of array
+                if tile_x == width - 1:
+                    edge0 = ResourceTileEdge(baseX + 131 + 104 * tile_x, baseY + 9 + 74 * tile_y)
+                    board.resource_tiles[tile_y][tile_x].edges[0] = edge0
+
+                # set tile edges
+                board.resource_tiles[tile_y][tile_x].edges[2] = edge2
+                board.resource_tiles[tile_y][tile_x].edges[3] = edge3
+
+                # set adjacent tile edges
+                if tile_y != height - 1:
+                    board.resource_tiles[tile_y + 1][tile_x].edges[0] = edge3
+                    if tile_x != width - 1:
+                        board.resource_tiles[tile_y + 1][tile_x + 1].edges[5] = edge2
